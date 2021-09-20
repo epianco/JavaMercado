14b4fbb9587ccd2be8f47b9efe0bf0c1687dc030
Edson Vinicius Piancó <vinicius.dsp19@gmail.com>
2021-09-19 21:46:35 -0300

    Add files via upload

---
+++ Mercado/src/model/bean/Produto.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+public class Produto {
+    private int id;
+    private String descricacao;
+    private int qtd;
+    private double preco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricacao() {
+        return descricacao;
+    }
+
+    public void setDescricacao(String descricacao) {
+        this.descricacao = descricacao;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    
+    
+}
